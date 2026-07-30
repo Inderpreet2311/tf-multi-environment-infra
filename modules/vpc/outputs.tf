30f2a6d72a69b0553b8c502c7b9379034dd74878
Inderpreet Singh <inderpreet2311@gmail.com>
2026-07-30 14:09:15 -0400

    modules

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,14 @@
+output "vpc_id" {
+    description = " VPC Id for pass on"
+    value = aws_vpc.vpc.id
+}
+
+output "public_subnet_id" {
+    description = " Public Subnet Id for pass on "
+    value = aws_subnet.public_subnet.id
+}
+
+output "private_subnet_id" {
+    description = "Private Subnet Id for pass on"
+    value = aws_subnet.private_subnet.id
+}
