df579268cc17a1ffcaa4c0c0ff1406a3240416cf
Inderpreet Singh <inderpreet2311@gmail.com>
2026-07-30 22:14:13 -0400

    modules

--- modules/EC2/main.tf
+++ modules/EC2/main.tf
@@ -20,3 +20,14 @@ resource "aws_security_group" "aws_security_group" {
         Name = "${var.name_prefix}-security-group"
     })
 }
+
+resource "aws_instance" "aws_instance" {
+    ami = "ami-0c02fb55956c7d316"
+    instance_type = var.instance_type
+    subnet_id = var.subnet_id
+    vpc_security_group_ids = [aws_security_group.aws_security_group.id]
+
+    tags = merge (var.common_tags, {
+        Name = "${var.name_prefix}-aws_instance"
+    })
+}
