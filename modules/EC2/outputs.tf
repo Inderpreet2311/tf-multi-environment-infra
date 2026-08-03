576c702244e6766887415b251c716b3364ddcce6
Inderpreet Singh <inderpreet2311@gmail.com>
2026-08-03 17:08:45 -0400

    modules

---
+++ modules/EC2/outputs.tf
@@ -0,0 +1,14 @@
+output "instance_id" {
+    description = "Outputs Id for reference"
+    value = aws_instance.aws_instance.id
+}
+
+output "public_ip" {
+    description = "Public IP for reference"
+    value = aws_instance.aws_instance.public_ip
+}
+
+output "security_group_id" {
+    description = " security group ID for reference"
+    value = aws_security_group.aws_security_group.id
+}
